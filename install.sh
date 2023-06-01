4ef6928281e6935fa6c208bfe021d1cc83d798e3
AN <support@kamtre.com>
2023-06-01 17:43:02 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 cd /
 
+echo "DEFAULT_VERSIONS+=llvm=15" >> /etc/make.conf
+echo "DEFAULT_VERSIONS+=openssl" >> /etc/make.conf
+
 zfs destroy -r zroot/pgdb
 zfs create -o mountpoint=/pgdb zroot/pgdb
 zfs create -o mountpoint=/pgdb/data zroot/pgdb/data
